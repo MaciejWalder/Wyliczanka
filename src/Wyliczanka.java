42807a50dfa6c83c02ff0c09b92ecf5b78ff8412
Maciej <walder_m@wp.pl>
2022-11-24 14:43:58 +0100

    Aplikacja Wyliczanka -HomeWork number 1

---
+++ src/Wyliczanka.java
@@ -0,0 +1,9 @@
+public class Wyliczanka {
+    public static void main(String[] args) {
+        System.out.println("Aplikacja wyliczanka:");
+
+        for (int i=0; i<10 ; i++) {
+            System.out.println(i+1);
+        }
+    }
+}
